0a53308b054bd932ea7d37a374a48152a83fcbfc
Raspalindes <surimagenmerida@gmail.com>
2025-09-02 22:07:41 +0200

    feat: update side menu options and improve HTML structure

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gift-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
-  styleUrl: './side-menu-options.component.css'
+  styleUrl: './side-menu-options.component.css',
 })
 export class GiftSideMenuOptionsComponent {
-
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
 }
