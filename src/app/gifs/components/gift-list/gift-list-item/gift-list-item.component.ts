afcda9740fb7216eb60dbe37b0d8ed9105596570
Raspalindes <surimagenmerida@gmail.com>
2025-09-05 20:15:54 +0200

    feat: implement gift list and item components with basic structure and tests

---
+++ src/app/gifs/components/gift-list/gift-list-item/gift-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gift-list-item',
+  imports: [],
+  templateUrl: './gift-list-item.component.html',
+  styleUrl: './gift-list-item.component.css'
+})
+export class GiftListItemComponent {
+
+}
